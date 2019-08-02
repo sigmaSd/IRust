812b072cc11511de9510f19dcc3b234f8ad17c72
Nbiba Bedis <bedisnbiba@gmail.com>
2019-08-02 21:30:25 +0100

    handle main panic

--- src/irust/format.rs
+++ src/irust/format.rs
@@ -9,13 +9,21 @@ pub fn format_eval_output(output: &str) -> Printer {
         // Consider this an error
         let lines_count = output.lines().count();
 
-        let actual_error = output
-            .lines()
-            .skip(1)
-            .take(lines_count - 8)
-            .collect::<Vec<&str>>()
-            .join("\n");
+        let actual_error: String = if main_panic(&output) {
+            // example:
+            // thread 'main' panicked at 'attempt to multiply with overflow',
+            let mut output: Vec<&str> = output.lines().nth(3).unwrap().split(',').collect();
+            output.pop();
 
+            output.join(",")
+        } else {
+            output
+                .lines()
+                .skip(1)
+                .take(lines_count - 8)
+                .collect::<Vec<&str>>()
+                .join("\n")
+        };
         eval_output.push(PrinterItem::new(actual_error, PrinterItemType::Err));
     } else {
         eval_output.push(PrinterItem::new(OUT.into(), PrinterItemType::Out));
@@ -32,4 +40,8 @@ pub fn format_eval_output(output: &str) -> Printer {
     }
 
     eval_output
+}
+
+fn main_panic(s: &str) -> bool {
+    s.contains("thread 'main' panicked")
 }
