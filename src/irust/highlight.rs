749a1716db66061df98a57d4e8cd6ef2cf6565f4
Nbiba Bedis <bedisnbiba@gmail.com>
2019-08-06 20:02:02 +0100

    improve highlight fn

--- src/irust/highlight.rs
+++ src/irust/highlight.rs
@@ -1,20 +1,21 @@
 use super::printer::{Printer, PrinterItem, PrinterItemType};
+use once_cell::sync::Lazy;
 use syntect::easy::HighlightLines;
 use syntect::highlighting::{Color, ThemeSet};
-use syntect::parsing::SyntaxSet;
+use syntect::parsing::{SyntaxReference, SyntaxSet};
 use syntect::util::LinesWithEndings;
 
-pub fn highlight(c: &str) -> Printer {
-    let ps = SyntaxSet::load_defaults_newlines();
-    let ts = ThemeSet::load_defaults();
+static PS: Lazy<SyntaxSet> = Lazy::new(SyntaxSet::load_defaults_newlines);
+static TS: Lazy<ThemeSet> = Lazy::new(ThemeSet::load_defaults);
+static SYNTAX: Lazy<&SyntaxReference> = Lazy::new(|| PS.find_syntax_by_extension("rs").unwrap());
 
-    let syntax = ps.find_syntax_by_extension("rs").unwrap();
-    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+pub fn highlight(c: &str) -> Printer {
+    let mut h = HighlightLines::new(&SYNTAX, &TS.themes["base16-ocean.dark"]);
 
     let mut printer = Printer::default();
 
     for line in LinesWithEndings::from(c) {
-        h.highlight(line, &ps)
+        h.highlight(line, &PS)
             .into_iter()
             .for_each(|(style, part)| {
                 let fg_color = match style.foreground {
