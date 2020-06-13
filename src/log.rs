c7160553d588c6c4c14701293308ae6c4ac53b55
Nbiba Bedis <bedisnbiba@gmail.com>
2020-06-13 20:54:58 +0100

    improve logging

--- src/log.rs
+++ src/log.rs
@@ -1,5 +1,5 @@
 #[macro_export]
-macro_rules! log {
+macro_rules! log_inner {
     ($($arg:tt)*) => (
         std::sync::Once::new().call_once(|| {
             use std::io::Write;
@@ -14,4 +14,27 @@ macro_rules! log {
         });
 
     );
+}
+
+#[macro_export]
+macro_rules! log {
+    () => {
+        $crate::log_inner!("[{}:{}]", file!(), line!());
+    };
+    ($val:expr) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                $crate::log_inner!("[{}:{}] {} = {:#?}",
+                    file!(), line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    // Trailing comma with single argument is ignored
+    ($val:expr,) => { $crate::log!($val) };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::log!($val)),+,)
+    };
 }
