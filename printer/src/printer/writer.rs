a685e3745153a592798ffb2220c2901869d212cd
Nbiba Bedis <bedisnbiba@gmail.com>
2021-04-24 14:27:49 +0100

    BugFix: Include the prompt size in calculation when clearing the screen

--- printer/src/printer/writer.rs
+++ printer/src/printer/writer.rs
@@ -122,7 +122,7 @@ impl<W: std::io::Write> Writer<W> {
         self.raw.clear(ClearType::All)?;
 
         cursor.set_starting_pos(0, 0);
-        cursor.goto(4, 0);
+        cursor.goto_input_start_col();
         cursor.reset_bound();
         Ok(())
     }
