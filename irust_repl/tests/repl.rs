3067c769e48c1bc43fd52bb199bf9dc551c02fef
Nbiba Bedis <bedisnbiba@gmail.com>
2021-05-06 10:49:04 +0100

    update test

--- irust_repl/tests/repl.rs
+++ irust_repl/tests/repl.rs
@@ -2,7 +2,7 @@ use irust_repl::*;
 
 #[test]
 fn repl() {
-    let mut repl = Repl::new(ToolChain::Stable, Executor::Sync).unwrap();
+    let mut repl = Repl::new(ToolChain::Stable).unwrap();
     repl.insert("let a = 4;");
     repl.insert("let b = 6;");
     assert_eq!(repl.eval("a+b").unwrap().output, "10");
