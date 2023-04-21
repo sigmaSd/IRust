8889673f5aa9495a86eec45f025194df4fa8b980
Nbiba Bedis <bedisnbiba@gmail.com>
2023-04-21 13:29:23 +0100

    first step

--- crates/irust_repl/examples/re/main.rs
+++ crates/irust_repl/examples/re/main.rs
@@ -1,62 +1,44 @@
-use irust_repl::{EvalConfig, Repl, DEFAULT_EVALUATOR};
+use irust_repl::Repl;
+use serde::{Deserialize, Serialize};
+use serde_json::Deserializer;
+use std::io;
 
-fn main() {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-
-    match args.len() {
-        0 => panic!("No code provided"), // error
-        1 => {
-            // code
-            eval(None, &args[0]);
-        }
-        2 => {
-            // deps + code
-            eval(Some(&args[0]), &args[1]);
-        }
-        _ => panic!("Extra arguments provided"), // extra arguments
-    }
+#[derive(Serialize, Deserialize)]
+struct Message {
+    code: String,
+}
+#[derive(Serialize, Deserialize)]
+struct Response {
+    result: String,
+    inserted: bool,
 }
 
-fn eval(deps: Option<&str>, code: &str) {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+    let mut deserializer = Deserializer::from_reader(reader).into_iter::<Message>();
+
     let mut repl = Repl::default();
-    if let Some(deps) = deps {
-        let deps: Vec<String> = split_args(deps.to_string());
-        repl.add_dep(&deps).unwrap().wait().unwrap();
-    }
-    let result = repl
-        .eval_with_configuration(EvalConfig {
-            input: code,
-            interactive_function: None,
-            color: true,
-            evaluator: &*DEFAULT_EVALUATOR,
-            compile_mode: irust_repl::CompileMode::Debug,
-        })
-        .unwrap();
-    println!("{}", result.output);
-}
 
-fn split_args(s: String) -> Vec<String> {
-    let mut args = vec![];
-    let mut tmp = String::new();
-    let mut quote = false;
-
-    for c in s.chars() {
-        match c {
-            ' ' => {
-                if !quote && !tmp.is_empty() {
-                    args.push(tmp.drain(..).collect());
-                } else {
-                    tmp.push(' ');
-                }
-            }
-            '"' => {
-                quote = !quote;
-            }
-            _ => tmp.push(c),
+    while let Some(json) = deserializer.next() {
+        let message = json?;
+        if message.code.ends_with(";") {
+            repl.insert(&message.code);
+            let output = serde_json::to_string(&Response {
+                result: "".to_string(),
+                inserted: true,
+            })?;
+            println!("{output}");
+        } else {
+            let result = repl.eval(message.code)?;
+            let output = serde_json::to_string(&Response {
+                result: result.output,
+                inserted: false,
+            })?;
+            println!("{output}");
         }
     }
-    if !tmp.is_empty() {
-        args.push(tmp);
-    }
-    args
+
+    Ok(())
 }
+
