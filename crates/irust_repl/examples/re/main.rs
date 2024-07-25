7b7c3b84259fc45ff800e62c5e0482a659998d8f
Nbiba Bedis <bedisnbiba@gmail.com>
2024-07-25 18:16:27 +0100

    improve errors ui

--- crates/irust_repl/examples/re/main.rs
+++ crates/irust_repl/examples/re/main.rs
@@ -1,7 +1,7 @@
-use irust_repl::Repl;
+use irust_repl::{EvalConfig, EvalResult, Repl, DEFAULT_EVALUATOR};
 use serde::{Deserialize, Serialize};
 use serde_json::Deserializer;
-use std::io;
+use std::{io, sync::OnceLock};
 
 #[derive(Serialize, Deserialize)]
 struct Message {
@@ -69,7 +69,27 @@ fn main() -> Result<()> {
                 println!("{output}");
             } else {
                 // eval here
-                let value = repl.eval(message.code)?.output;
+                let EvalResult {
+                    output: value,
+                    status,
+                } = repl.eval_with_configuration(EvalConfig {
+                    input: message.code,
+                    interactive_function: None,
+                    color: true,
+                    evaluator: &*DEFAULT_EVALUATOR,
+                    compile_mode: irust_repl::CompileMode::Debug,
+                })?;
+
+                // It errored, format the error and send it
+                if !status.success() {
+                    let output = serde_json::to_string(&Action::Eval {
+                        // NOTE: make show warnings configurable
+                        value: format_err(&value, false, &repl.cargo.name),
+                        mime_type: MimeType::PlainText,
+                    })?;
+                    println!("{output}");
+                    return Ok(());
+                }
 
                 // EVCXR
                 if value.starts_with("EVCXR_BEGIN_CONTENT") {
@@ -109,6 +129,9 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+// The next functions are extracted from irust
+// They should be extracted to a separate common crate
+
 pub fn is_a_statement(buffer_trimmed: &str) -> bool {
     match buffer_trimmed
         .split_whitespace()
@@ -131,5 +154,89 @@ pub fn is_a_statement(buffer_trimmed: &str) -> bool {
         // struct B{}
         [tag, ..] if tag.starts_with('#') => true,
         _ => false,
+    }
+}
+
+static NO_COLOR: OnceLock<bool> = OnceLock::new();
+/// Have the top precedence
+fn no_color() -> bool {
+    *NO_COLOR.get_or_init(|| std::env::var("NO_COLOR").is_ok())
+}
+pub fn format_err<'a>(original_output: &'a str, show_warnings: bool, repl_name: &str) -> String {
+    const BEFORE_2021_END_TAG: &str = ": aborting due to ";
+    // Relies on --color=always
+    const ERROR_TAG: &str = "\u{1b}[0m\u{1b}[1m\u{1b}[38;5;9merror";
+    const WARNING_TAG: &str = "\u{1b}[0m\u{1b}[1m\u{1b}[33mwarning";
+
+    // These are more fragile, should be only used when NO_COLOR is on
+    const ERROR_TAG_NO_COLOR: &str = "error[";
+    const WARNING_TAG_NO_COLOR: &str = "warning: ";
+
+    let go_to_start = |output: &'a str| -> Vec<&'a str> {
+        if show_warnings {
+            output
+                .lines()
+                .skip_while(|line| !line.contains(&format!("{repl_name} v0.1.0")))
+                .skip(1)
+                .collect()
+        } else {
+            output
+                .lines()
+                .skip_while(|line| {
+                    if no_color() {
+                        !line.starts_with(ERROR_TAG_NO_COLOR)
+                    } else {
+                        !line.starts_with(ERROR_TAG)
+                    }
+                })
+                .collect()
+        }
+    };
+    let go_to_end = |output: Box<dyn Iterator<Item = &str>>| -> String {
+        if show_warnings {
+            output
+        } else {
+            Box::new(output.take_while(|line| {
+                if no_color() {
+                    !line.starts_with(WARNING_TAG_NO_COLOR)
+                } else {
+                    !line.starts_with(WARNING_TAG)
+                }
+            }))
+        }
+        .collect::<Vec<_>>()
+        .join("\n")
+    };
+
+    let handle_error = |output: &'a str| {
+        go_to_start(output)
+            .into_iter()
+            .take_while(|line| !line.contains(BEFORE_2021_END_TAG))
+    };
+    let handle_error_2021 = |output: &'a str| {
+        go_to_start(output)
+            .into_iter()
+            .rev()
+            .skip_while(|line| !line.is_empty())
+            .collect::<Vec<_>>()
+            .into_iter()
+            .rev()
+    };
+
+    let output: Box<dyn Iterator<Item = &str>> = if original_output.contains(BEFORE_2021_END_TAG) {
+        Box::new(handle_error(original_output))
+    } else {
+        Box::new(handle_error_2021(original_output))
+    };
+
+    let formatted_error = go_to_end(output);
+    // The formatting logic is ad-hoc, there will always be a chance of failure with a rust update
+    //
+    // So we do a sanity check here, if the formatted_error is empty (which means we failed to
+    // format the output), ask the user to open a bug report with the original_output
+    if !formatted_error.is_empty() {
+        formatted_error
+    } else {
+        format!("IRust: failed to format the error output.\nThis is a bug in IRust.\nFeel free to open a bug-report at https://github.com/sigmaSd/IRust/issues/new with the next text:\n\noriginal_output:\n{original_output}")
     }
 }
